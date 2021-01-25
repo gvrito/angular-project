8259c320d279935bbbdd6cac173a3e3d49f544cc
Gvrito123 <gvritagio@gmail.com>
2021-01-25 13:41:20 +0400

    feat(3.HttpClient) finished pagination

--- src/app/employees/employees-list/employees-list.component.ts
+++ src/app/employees/employees-list/employees-list.component.ts
@@ -8,42 +8,55 @@ import { CrudService, Employee } from '../crud.service';
 })
 export class EmployeesListComponent implements OnInit {
   employees:Employee[] = [];
-  employeesOnPage:Employee[] = [];
-  pages:number = 1;
+  pages = new Array(1);
   employeeNumber:number;
   currentPage = 1;
+  isFocusMode = false;
 
   constructor(private crud:CrudService) { }
 
   ngOnInit(): void {
     this.updateList();
     this.crud.refreshList.subscribe(()=> {
+      this.paginate(this.currentPage);
       this.updateList();
-    })
+    });
   }
 
   updateList(){
+    this.paginate(this.currentPage);
     this.crud.readAllEmployees().subscribe((data)=> {
-      this.employees = data;
       this.employeeNumber = data.length;
-      this.pages = Math.ceil(this.employeeNumber / 10);
-      if(this.pages > 1){
+      this.pages = new Array(Math.ceil(this.employeeNumber / 10));
+      if(this.pages.length > 1){
       }
     });
   }
 
-  paginate(totalItems, currentPage){
-
+  paginate(currentPage:number){
+    this.crud.readPage(currentPage).subscribe((data)=> {
+      this.isFocusMode = false;
+      this.employees = data;
+      this.currentPage = currentPage;
+    })
   }
 
   delete(id:number){
     this.crud.removeEmployee(id).subscribe((data)=> {
-      this.updateList()
+      this.updateList();
     })
   }
 
   edit(event){
     this.crud.isEditMode.emit(event);
+  }
+
+  focusEmployee(id:number){
+    this.employees = [];
+    this.isFocusMode = true;
+    this.crud.readEmployee(id).subscribe((data)=> {
+      this.employees.push(data);
+    })
   }
 
 }
